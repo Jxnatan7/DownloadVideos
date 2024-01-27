99348f5ea11e9c1cd52aedb137c5d7d744744ccb
jxnatan7 <jgabrieldesouzasantos@gmail.com>
2024-01-26 22:39:26 -0300

    :cyclone: refactor: Altera response da rota '/download' para apenas retornar o áudio.

--- youtubeDownloader.js
+++ youtubeDownloader.js
@@ -17,24 +17,11 @@ app.get('/download', async (req, res) => {
     try {
         const URL = req.query.URL;
         res.header('Content-Disposition', 'attachment; filename="audio.mp3"');
-        const outputPath = 'audio.mp3';
         
         const response = await ytdl(URL, { format: 'mp3', filter: 'audioonly' });
         response.pipe(res);
 
-        const audiosFolderPath = path.join(__dirname, 'audios');
-        
-        if (!fs.existsSync(audiosFolderPath)) {
-            fs.mkdirSync(audiosFolderPath);
-        }
-
-        const fullOutputPath = path.join(audiosFolderPath, outputPath);
-
-        response.on('end', () => {
-            console.log(`Download concluído: ${fullOutputPath}`);
-        });
-
-        response.pipe(fs.createWriteStream(fullOutputPath));
+        return response;
     } catch (error) {
         console.error('Erro durante o download:', error.message);
         res.status(500).send('Erro durante o download');
