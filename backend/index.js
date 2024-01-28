7725755ed42b558c884faa769577738763f84651
jxnatan7 <jgabrieldesouzasantos@gmail.com>
2024-01-27 23:26:35 -0300

    :wrench: fix: Corrige configurações de origin e credentials do cors.

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,7 @@ app.use((req, res, next) => {
 
 
 app.use(cors({
-    origin: 'https://jxnatan7.github.io/DownloadVideos/site/index.html',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }));
 
 app.listen(8080, () => {
