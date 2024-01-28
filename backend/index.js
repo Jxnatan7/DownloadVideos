39fa1dbf9d35ca1dcbc0c6b7375ad82d6e640687
jxnatan7 <jgabrieldesouzasantos@gmail.com>
2024-01-27 23:20:13 -0300

    :wrench: fix: Corrige configurações de credentials do cors.

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,7 @@ app.use((req, res, next) => {
 
 
 app.use(cors({
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }));
 
 app.listen(8080, () => {
