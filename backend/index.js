d9f7743f88a0e192ab7d5312c2ba4127a96b705c
jxnatan7 <jgabrieldesouzasantos@gmail.com>
2024-01-28 17:58:23 -0300

    :construction: wip: Testa retorno de vídeo da API.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
-const ffmpeg = require('fluent-ffmpeg');
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg');
-
-ffmpeg.setFfmpegPath(ffmpegPath.path);
-
 const app = express();
+
 const PORT = 4000;
 
 const corsOptions = {
@@ -38,28 +34,21 @@ app.get('/download', async (req, res) => {
         }
 
         const stream = ytdl(URL, { format: 'mp4', quality: 140 });
+
+        const videoTitle = info.videoDetails.title.replace(/[^\w\s\-]/g, '');
+
+        res.header('Content-Disposition', `attachment; filename="${videoTitle}.mp3"`);
+
+        stream.on('end', () => {
+            console.info(`Conversion finished for ${videoTitle}`);
+        });
+
+        stream.on('error', (err) => {
+            console.error('Error during conversion:', err);
+            res.status(500).json({ error: 'Error during conversion', message: err.message });
+        });
+
         stream.pipe(res, { end: true });
-
-//         const ffmpegCommand = ffmpeg();
-//         ffmpegCommand.input(stream);
-//         ffmpegCommand.audioCodec('libmp3lame');
-//         ffmpegCommand.audioBitrate(192);
-//         ffmpegCommand.format('mp3');
-
-//         const videoTitle = info.videoDetails.title.replace(/[^\w\s\-]/g, '');
-// ;
-//         res.header('Content-Disposition', `attachment; filename="${videoTitle}.mp3"`);
-
-//         ffmpegCommand.on('end', () => {
-//             console.info(`Conversion finished for ${videoTitle}`);
-//         });
-
-//         ffmpegCommand.on('error', (err) => {
-//             console.error('Error during conversion:', err);
-//             res.status(500).json({ error: 'Error during conversion', message: err.message });
-//         });
-
-        // ffmpegCommand.pipe(res, { end: true });
     } catch (error) {
         console.error('Error during download:', error);
         res.status(500).json({ error: 'Error during download', message: error.message });
